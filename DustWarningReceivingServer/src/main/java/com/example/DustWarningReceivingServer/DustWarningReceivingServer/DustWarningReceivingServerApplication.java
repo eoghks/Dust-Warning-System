c9635526fe643f81d5ec7c23b585d2c71bc5365e
eoghks <zhfldk7316@naver.com>
2024-11-26 13:23:37 +0900

    1. 경보 수신 서버 init

---
+++ DustWarningReceivingServer/src/main/java/com/example/DustWarningReceivingServer/DustWarningReceivingServer/DustWarningReceivingServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.DustWarningReceivingServer.DustWarningReceivingServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DustWarningReceivingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DustWarningReceivingServerApplication.class, args);
+	}
+
+}
---
+++ DustWarningReceivingServer/src/test/java/com/example/DustWarningReceivingServer/DustWarningReceivingServer/DustWarningReceivingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DustWarningReceivingServer.DustWarningReceivingServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DustWarningReceivingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
